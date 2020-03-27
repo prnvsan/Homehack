05c7c32d7f8552689b267131ec4d0878c31401fb
GeorgeSabu-mec <georgesabu36@gmail.com>
2020-03-27 13:04:09 +0530

    Cloned prnv repo

--- questions.js
+++ questions.js
@@ -26,8 +26,8 @@ var questions = [
           $(this).text(questions[i]).fadeIn();
         });
        if(i==questions.length){
-       $(".QuestionBox button").fadeOut("slow");
-   
+       $("button").fadeOut("slow");
+    
        if(choices.indexOf("y")>=0)
           result="You are at high risk of having contracted COVID-19,follow government guidelines and self isolate yourself.";
        else
