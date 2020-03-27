561bae348df90e7a05456af2cddf8788ddd44752
GeorgeSabu-mec <georgesabu36@gmail.com>
2020-03-27 11:11:53 +0530

    Fixed issue of all buttons fading

--- questions.js
+++ questions.js
@@ -26,7 +26,7 @@ var questions = [
           $(this).text(questions[i]).fadeIn();
         });
        if(i==questions.length){
-       $("button").fadeOut("slow");
+       $(".QuestionBox button").fadeOut("slow");
     
        if(choices.indexOf("y")>=0)
           result="You are at high risk of having contracted COVID-19,follow government guidelines and self isolate yourself.";
